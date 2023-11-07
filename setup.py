cba223b82285edc9e26fec0687b733c70d45a48e
bruce <cnhxunoo@gmail.com>
2023-11-07 15:27:55 +0800

    update verion in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="retroapi",
-    version="0.2.0",
+    version="0.3.0",
     description="retroapi for retrosynthesis routes and exploring reaction conditions",
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
